f845f92e2c0e50be7e4c1d5b2b5f8e90cd0dc3ab
Fusion Solutions <solutions.fusion@gmail.com>
2017-10-22 21:56:08 +0530

    Updated Account sql script

--- sql/fintech.sql
+++ sql/fintech.sql
@@ -314,21 +314,3 @@ UNLOCK TABLES;
 
 
 
-
-update account set password = 'ea7cb1c6afc7d2d397f3c63c51b21a41' where account_name='QuantResearch';
-update account set password = '2d0156a7df679126a093340f5067f0c6' where account_name='FinBackTesting';
-update account set password = 'c8cd1446019a02aadcc17c2e2405a01e' where account_name='mkyong';
-
-
-
-
-TRUNCATE TABLE `app_role`;
-TRUNCATE TABLE `account_roles`;
-
-INSERT INTO `app_role` (`code`, `name`) VALUES
-('ROLE_ADMIN', 'ROLE_ADMIN');
-
-INSERT INTO `account_roles` (`account_id`, `role`) VALUES
-(1, 'ROLE_ADMIN'),
-(2, 'ROLE_ADMIN'),
-(3, 'ROLE_ADMIN');
